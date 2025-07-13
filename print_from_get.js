845e239efbb478c7334157c51f78ba64c1611f67
vsandrey101 <58606923+vsandrey101@users.noreply.github.com>
2025-07-13 13:28:28 +0300

    Add files via upload

---
+++ print_from_get.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    const urlParams = new URLSearchParams(window.location.search);
+    name = urlParams.get('name');
+    msg = urlParams.get('message');
+    const holder = document.getElementById('to_change');
+    if (name !== "null" && msg !== "null"){
+        holder.innerText = `Hello ${name}! ${msg}!`;
+    } else {
+        holder.innerText = "Не были переданы значения!"
+    }
+
+}
