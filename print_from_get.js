dbe7df2ff9dd12dd1e4f5eee25fbb49dab62017b
vsandrey101 <58606923+vsandrey101@users.noreply.github.com>
2025-07-13 14:08:46 +0300

    Update print_from_get.js

--- print_from_get.js
+++ print_from_get.js
@@ -1,10 +1,12 @@
 window.onload = function(){
     const urlParams = new URLSearchParams(window.location.search);
-    name = urlParams.get('name');
+    in_name = urlParams.get('name');
     msg = urlParams.get('message');
     const holder = document.getElementById('to_change');
-    if (name !== 'null' && msg !== 'null'){
-        holder.innerText = `Hello ${name}! ${msg}!`;
+    console.log(in_name);
+    console.log(msg);
+    if (in_name != null && msg != null){
+        holder.innerText = `Hello ${in_name}! ${msg}!`;
     } else {
         holder.innerText = "Не были переданы значения!"
     }
